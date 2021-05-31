d87b6bc0e572bb5c69cb70077661903ac179574d
liuanguang <liuanguang@cvte.com>
2021-05-31 22:10:43 +0800

    feat: using vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  base: "/JavaFX-For-Dummies-zh/",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  title: 'JavaFX For Dummies',
+  description: '跟阿呆学 JavaFX',
+  // 主题配置
+  themeConfig: {
+    logo: '/favicon.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '购买本书', link: 'https://www.amazon.com/JavaFX-Dummies-Doug-Lowe/dp/1118385349' },
+    ],
+    // 仓库相关
+    repo: 'Angus-Liu/JavaFX-For-Dummies-zh',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLinks: true,
+    editLinkText: '帮助改善翻译！',
+    // 侧边栏配置
+    sidebar: [
+      '/Introduction',
+      '/Part-1',
+      '/Chapter-1',
+      '/Chapter-2',
+      '/Chapter-3',
+      '/Chapter-4',
+      '/Chapter-5',
+      '/Chapter-6',
+      '/Part-2',
+      '/Chapter-7',
+      '/Chapter-8',
+      // '/Chapter-9',
+      '/Chapter-10',
+      '/Part-3',
+      '/Chapter-11',
+      '/Chapter-12',
+      '/About-the-Author',
+    ]
+  },
+  // markdown 配置
+  markdown: {
+    // lineNumbers: true
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,23 @@
+---
+home: true
+heroImage: '/cover.jpg'
+heroText: JavaFX For Dummies
+tagline: 跟阿呆学 JavaFX
+actionText: 开始阅读 →
+actionLink: /Chapter-1
+features:
+- title: 基础
+  details: 安装 JavaFX 开发工具，包括 JavaFX SDK、Java SE JDK 和 NetBeans。
+- title: 教程
+  details: 开发桌面、移动和浏览器应用。
+- title: 拓展
+  details: 在 JavaFX 中使用 HTML5 和 CSS3
+footer: CC-BY-SA-4.0 License | Copyright © 2021-present Angus-Liu
+---
+
+## 关于作者
+
+::: tip 道格·洛韦（Doug Lowe）
+发明 Java 的人还在读小学时就开始写计算机编程相关的书籍了。他写过关于 COBOL、FORTRAN、Visual Basic、IBM 大型机、中型系统、PC、网络编程的书，可能还有一些他完全忘记了。他是 30 多本 *For Dummies* 系列书籍的作者，包括：《Java All-In-One For Dummies, 4th Edition》、《Java For Dummies Quick Reference》、《Networking For Dummies, 10th Edition》、《Networking All-In-One For Dummies, 4th Edition》、《PowerPoint 2013 For Dummies》和《Electronics All-In-One For Dummies》。他住在阳光明媚的前全美城市弗雷斯诺，加利福尼亚。他的座右铭是：“Buster Posey Got Out, Why Can’t I?”
+:::
+
